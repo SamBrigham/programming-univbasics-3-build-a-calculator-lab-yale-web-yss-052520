013295a8e58528ff7bc60fa517ac14ffafa17193
Samuel Brigha <sam.brigham@yale.edu>
2020-05-09 17:53:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+puts (num1 + num2)
 end
 
 def subtraction(num1, num2)
-
+subtracting = num1 - num2
+puts subtracting
 end
 
 def division(num1, num2)
-
+dividing = num1 / num2
+puts dividing
 end
 
 def multiplication(num1, num2)
-
+multiplying = num1 * num2
+puts multiplying
 end
 
 def modulo(num1, num2)
-
+moduling = num1 % num2
+puts moduling
 end
 
 def square_root(num)
-
+squarerooting = num ** 0.5
+puts squarerooting
 end
+
